b2a36f636a2d85b56e394e438b62e0083983cca5
Emmanuel Dushime <64155652+dushimeemma@users.noreply.github.com>
2025-02-14 16:08:00 +0200

    Delete todo directory

--- todo/src/main/java/com/todo/todo/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.todo.todo.repository;
-
-import com.todo.todo.models.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
